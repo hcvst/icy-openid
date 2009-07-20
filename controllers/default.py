07d9d7d1be113db20c29f178f6641daa2e951b42
Hans Christian v. Stockhausen <hc@vst.io>
2009-07-20 05:53:47 +0200

    5:55 morning commit

--- controllers/default.py
+++ controllers/default.py
@@ -1,4 +1,6 @@
 def index():
     posts = []
-    posts.append(format_post('Welcome to ICY openid', 'your openid provider', P('blah '*500)))
+    posts.append(format_post('Welcome to ICY openid', 'your openid provider', P('openID is single-sign-on for the web. '*10 )))
+    posts.append(format_post('What is openID?', 'single-sign-on for the web', P('openID is single-sign-on for the web. '*10 )))
+    posts.append(format_post('Who uses openID?', 'single-sign-on for the web', P('openID is single-sign-on for the web. '*10 )))
     return dict(posts=posts)
